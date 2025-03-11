b0952738ce6f43c6128ff7644bf9d63de84174b9
Sophia Galvão <159277699+sophiaglv@users.noreply.github.com>
2025-03-11 16:30:12 -0300

    exercicio01

---
+++ doisBotoes1103/js/script.js
@@ -0,0 +1,15 @@
+function quadrado(){
+    const botao = document.createElement('button'); 
+
+    botao.classList.add('botao');
+
+    document.body.appendChild(botao);
+}
+
+function cor(){
+    const botao = document.createElement('button');
+
+    botao.classList.add('cor'); 
+
+    document.body.appendChild(botao); 
+}
